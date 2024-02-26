5023dbe2471a37f24ec90f196768e33a41977129
Vadim <vadim.novitskiy.98@gmail.com>
2024-02-26 12:27:57 +0200

    create websocket service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { WebSocketService } from './core/services/web-socket.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+  @ViewChild('localVideo') localVideo!: ElementRef<HTMLVideoElement>;
+  @ViewChild('remoteVideo') remoteVideo!: ElementRef<HTMLVideoElement>;
 
-  startCall() {
-    const videoMedia = navigator.mediaDevices.getUserMedia({
-      video: {
-        width: {ideal: 720},
-        height: {ideal: 480}
-      }, 
-      audio: true
-    });
+  connected = false;
 
-    console.log(videoMedia);
+  constructor(private webSocketService: WebSocketService) {}
+
+  ngOnInit(): void {
+  }
+
+  toggleConnection() {
+    if (this.connected) {
+      this.webSocketService.disconnect();
+    } 
+    this.webSocketService.connect();
+
+    this.connected !== this.connected;
+  }
+
+  startEchoTest() {
+    this.webSocketService.start();
   }
 }
--- src/app/core/services/web-socket.service.ts
+++ src/app/core/services/web-socket.service.ts
@@ -1,16 +1,56 @@
 import { Injectable } from '@angular/core';
-import { Socket } from 'ngx-socket-io';
+import { io, Socket } from 'socket.io-client';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebSocketService {
-  private webSocket!: Socket;
+  private socket!: Socket;
 
   constructor() {
-    this.webSocket = new Socket({
-      url: '',
-      options: {}
+    this.scheduleConnection();
+  }
+
+  scheduleConnection() {
+    let task: ReturnType<typeof setTimeout> | null = null;
+    const timeout = 5000;
+  
+    return () => {
+      if (task) return;
+      console.log('scheduled starting in ' + timeout + ' ms');
+      task = setTimeout(() => {
+        this.start();
+        task = null;
+      }, timeout);
+    };
+  }
+
+  connect() {
+    this.socket = io('https://localhost:4443', {
+      rejectUnauthorized: false,
+      autoConnect: false,
+      reconnection: false
+    });
+
+    this.socket.connect();
+
+    this.socket.on('connect', () => {
+      console.log('socker connected');
+      this.scheduleConnection();
     })
+  }
+
+  start() {
+    this.socket.emit('start', {
+      _id: this.getId()
+    });
+  }
+
+  disconnect() {
+    this.socket.disconnect();
+  }
+
+  private getId() {
+    return Math.floor(Number.MAX_SAFE_INTEGER * Math.random());
   }
 }
